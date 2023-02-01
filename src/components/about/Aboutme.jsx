b1ab561f0c45f9e36b578668fc21822bbb0b6a22
Renatebh <renate.hem@gmail.com>
2023-02-01 15:02:03 +0100

    added new content to about me

--- src/components/about/Aboutme.jsx
+++ src/components/about/Aboutme.jsx
@@ -6,7 +6,7 @@ const Aboutme = () => {
     <div>
       <HeadingOne text="Om meg" />
       <p>
-        Jeg er en 39 år gammel person bosatt i Tjølling sammen med min mann og
+        {/* "Jeg er en 39 år gammel person bosatt i Tjølling sammen med min mann og
         to barn på henholdsvis 9 og 11 år. Jeg har alltid vært fascinert av
         IT-bransjen og har ambisjoner om å jobbe innen dette feltet. Som
         tidligere leder har jeg evner til å hjelpe andre og bygge positive
@@ -19,7 +19,12 @@ const Aboutme = () => {
         og brukergrensesnitt, og har oppnådd høye karakterer på eksamen i dette
         faget. I løpet av sommeren har jeg samarbeidet med en klassekamerat om å
         utvikle et prosjekt for et bilutleiefirma i Oslo, ved hjelp av React.
-        Jeg jobber også deltid som mentor og lærerassistent ved akademiet.
+        Jeg jobber også deltid som mentor og lærerassistent ved akademiet."  */}
+        Jeg har en bred teknisk bakgrunn innen webutvikling og behersker en
+        rekke verktøy og teknologier, inkludert React, JavaScript, HTML, CSS,
+        Node, SQLite, Adobe XD og Git. Dette gir meg en sterk grunn for å løse
+        mange forskjellige oppgaver innen bransjen. Jeg har også erfaring med
+        prosjektarbeid, noe som gjør meg til en kapabel og allsidig kandidat.
       </p>
     </div>
   );
